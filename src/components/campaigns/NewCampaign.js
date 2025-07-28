2d940603471dc2bba7e2d73d1c5cdb79f7f6bcdf
Rushikesh Manke <rushikeshmk2000@gmail.com>
2025-07-28 18:18:29 +0530

    url updated

--- src/components/campaigns/NewCampaign.js
+++ src/components/campaigns/NewCampaign.js
@@ -47,7 +47,7 @@ export default function NewCampaign() {
             formData.append("targetAmt", formInput.target);
             formData.append("documentFile", documentFile);
 
-            const response = await fetch("http://localhost:8081/api/campaigns", {
+            const response = await fetch("http://localhost:8081/api/campaign/create", {
                 method: "POST",
                 body: formData,
             });
